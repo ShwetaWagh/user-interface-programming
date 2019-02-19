108801b9780f0e969bb57a7958d1a5367f50de10
ShwetaWagh <47498387+ShwetaWagh@users.noreply.github.com>
2019-02-19 11:30:08 +0100

    Update loginscreen(normal).js

--- UIP Project/customer/loginscreen(normal).js
+++ UIP Project/customer/loginscreen(normal).js
@@ -1,9 +1,32 @@
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Username must be filled out");
-        return false;
+function validateForm()
+{
+    var a = document.myForm.user.value;
+    var b = document.myForm.pass1.value;
 
+    if(a=="")
+    {
+        alert("Enter the username");
+        return false;
     }
+    if(!isNaN(a))
+    {
+        alert("Please enter only character");
+        return false;
+    }
+    if((a.length < 4)||(a.length > 12))
+    {
+        alert("Character must be 4 to 12 ");
+    }
+    else if(b=="")
+    {
+        alert("Enter the password");
+        return false;
+    }
+    else if(b.length<6){
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+
+
 }
 
