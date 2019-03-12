dc4cba227554337e1235e00080f7f4aa5e441de7
peech1224 <46927068+peech1224@users.noreply.github.com>
2019-03-12 01:10:55 +0100

    Add files via upload

--- customer v2.0/en/customer_confirm_order.php
+++ customer v2.0/en/customer_confirm_order.php
@@ -19,43 +19,100 @@ $total=0;
 
     </head>
 
-    <body class="customer_index_body">
+    <body class="customer_confirm_body">
+        
 
-        <div class="customer_header">
-            <img class="customer_language" src="../Images/language_icon.png" alt="Change Language">
-
-        </div>
-
-        <div>
+        <div><!-- Start of body content -->
+            
             <div class="customer_row">
-                <div class="customer_confirm_order_menu">
-                    <a href="customer_menu.php"><img src="../Images/menu_btn_left.png"></a>
-                </div>
-
-                <div class="customer_confirm_order_left">
-
-                </div>
-
-
-                <div class="customer_col_70">
-                    <div class="customer_col_70 customer_confirm_order_right">
-                        <span class="customer_confirm_order_h1">Order Confirmation</span>
-
-                        <a href="customer_pay.php"><button class="customer_confirm_order_button">PAY</button></a>
-                        <a href="customer_vip_login.html"><button class="customer_confirm_order_button">VIP PAY</button></a>
-
-
-                        <hr>
-                        <div id="show_conform_list"></div>
+                
+                <div class="customer_col_30">  <!-- left side content-->
+                    
+                    <div class="customer_confirm_left">
                         
-
+                        <a href="customer_vip_login.html"><img class="customer_vip_login_btn" src="../Images/VIP_Logo_login.png"></a>
+                    
+                    
+                        <div>
+                            <a href="customer_menu.php"><img src="../Images/menu_btn_left.png"></a>
+                        </div>
+                        
                     </div>
 
+                </div>          
+                
+                
+                <div class="customer_confirm_right_bg">
+                    
+                        <div class="customer_header"><!-- Header -->
+                            <img class="customer_language" src="../Images/language_icon.png" alt="Change Language">
+
+                        </div>
+                    
+                    <div class="customer_col_70"> <!-- right side content-->
+
+
+
+
+                        <div class="customer_confirm_order_right">
+                            
+                            <p class="customer_confirm_order_h1 align_center">Order Confirmation : Order number xxxxx</p>
+
+                            
+                            <div><!-- put row of order here-->
+                                <table class="customer_confirm_order_list vertical_align">
+                                    
+                                    <hr/>
+                                    <!-- One item on the list-->
+                                    <tr>
+                                        <td>1. xxx xxxxx ##</td>
+                                        
+                                        <td class="align_center"><img src="../Images/minus_icon.png"></td>
+                                        
+                                        <td class="align_center">1</td> 
+                                        
+                                        <td class="align_center"><img src="../Images/plus_icon.png">
+                                        </td>
+                                        
+                                        <td class="align_right">75.00 kr</td>
+                                    
+                                    </tr>
+                              
+                                    <!-- End one item on the list-->
+                                
+                                
+                                </table>
+                            
+                            
+                            
+                            </div>
+                            
+
+                            
+
+                            <div id="show_conform_list"></div>
+                            
+                            
+                            
+                            <br>
+                            
+                            
+                            <button class="customer_confirm_order_button" action="customer_pay.php">Pay
+                            </button>
+                           
+                        </div>
+
+
+                    </div>
+                
                 </div>
+                
             </div>
 
 
-        </div>
+        </div> <!-- End of body content -->
+        
+
         <script>
             showList();
 
